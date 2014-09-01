6a7d9846de45ef6dbd7bcc318b852d609feaf17a
Michael <Michael@Michael-Pc>
2014-09-01 12:44:32 +0200

    Added world's simplest web-server

--- src/firsthttpserver1/FirstHttpServer1.java
+++ src/firsthttpserver1/FirstHttpServer1.java
@@ -9,7 +9,11 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 
@@ -24,21 +28,26 @@ public class FirstHttpServer1 {
      */
     static int port = 8080;
     static String ip = "127.0.0.1";
+    static String contentFolder = "public/";
+    
 
     public static void main(String[] args) throws IOException {
-        if (args.length >= 2) {
+        if (args.length >= 3) {
             ip = args[0];
             port = Integer.parseInt(args[1]);
+            contentFolder = args[2];
         }
         InetSocketAddress i = new InetSocketAddress(ip, port); //localhost - 127.0.0.1
         HttpServer server = HttpServer.create(i, 0);
         server.createContext("/welcome", new WelcomeHandler());
         server.createContext("/headers", new HeadersHandler());
+        server.createContext("/pages/", new PagesHandler(contentFolder));
         server.setExecutor(null);
         server.start();
         System.out.println("Started the server, listening on:");
         System.out.println("port: " + port);
         System.out.println("ip: " + ip);
+        System.out.println("pages: " + contentFolder);
     }
 
     static class WelcomeHandler implements HttpHandler {
@@ -86,10 +95,9 @@ public class FirstHttpServer1 {
             sb.append("<th>Value</th>");
             sb.append("</tr>");
             for (String key : he.getRequestHeaders().keySet()) {
-                //he.getRequestHeaders().get(key);
                 sb.append("<tr>");
-                sb.append("<td>" + key);
-                sb.append("<td>" + he.getRequestHeaders().get(key));
+                sb.append("<td>" + key + "</td>");
+                sb.append("<td>" + he.getRequestHeaders().get(key) + "</td>");
                 sb.append("</tr>");
             }
             sb.append("</body>\n");
@@ -102,6 +110,30 @@ public class FirstHttpServer1 {
             try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
                 pw.print(response);
             };
+        }
+    }
+
+    static class PagesHandler implements HttpHandler {
+        String contentFolder;
+
+        private PagesHandler(String contentFolder) {
+            this.contentFolder = contentFolder;
+        }
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            File file = new File(contentFolder + "index.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend, 0, bytesToSend.length);
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            }
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend, 0, bytesToSend.length);
+            }
         }
     }
 }
